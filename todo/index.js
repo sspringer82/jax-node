5e0b2394648953fc0dbfea3a337a7f6cd981cf6b
Sebastian Springer <info@sebastian-springer.com>
2018-11-05 14:36:06 +0100

    access params in url

--- todo/index.js
+++ todo/index.js
@@ -5,5 +5,9 @@ const { listAction } = require('./todo.controller');
 const router = express.Router();
 
 router.get('/', listAction);
+router.get('/:id', (req, res) => {
+  console.log(req.params.id);
+  res.send('Hello Client');
+});
 
 module.exports = router;
