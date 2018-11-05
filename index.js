59fe52c6523f4219d09339564a012340b58a8862
Sebastian Springer <info@sebastian-springer.com>
2018-11-05 14:34:09 +0100

    logger

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const todo = require('./todo');
 
 const app = express();
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  {
+    flags: 'a',
+  },
+);
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.set('view engine', 'ejs');
 
